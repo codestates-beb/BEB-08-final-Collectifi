f4073e3541ec59b35952a626815cb157e2217975
bloud411 <bloud411@gmail.com>
2023-04-14 09:49:00 +0900

    nft one get

--- server/routes/game.route.ts
+++ server/routes/game.route.ts
@@ -1,9 +1,16 @@
 import express from 'express';
-import {game_post, game_post1} from '../controllers/game.controller';
+import {
+  game_fund_post,
+  game_post,
+  game_post1,
+  game_reward_post,
+} from '../controllers/game.controller';
 
 const router = express.Router();
 
 router.post('/', game_post);
 router.post('/admin', game_post1);
+router.post('/fund', game_fund_post);
+router.post('/reward', game_reward_post);
 
 export default router;
