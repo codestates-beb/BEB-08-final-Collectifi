67b62f99c39763f2997043c755bd0ab799145f47
soma <macaronieyes@gmail.com>
2023-04-18 00:11:46 +0900

    feat: fallback page

--- client/src/pages/MarketPage.tsx
+++ client/src/pages/MarketPage.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import {Suspense} from 'react';
 import {ErrorBoundary} from 'react-error-boundary';
 import Market from '../components/market/Market';
+import Fallback from '../components/Fallback';
 
 const MarketPage = () => {
   return (
-    <ErrorBoundary fallback={<div>Error!</div>}>
+    <ErrorBoundary fallback={<Fallback />}>
       <Suspense>
         <Market />
       </Suspense>
--- client/src/pages/UserPage.tsx
+++ client/src/pages/UserPage.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import {Suspense} from 'react';
 import {ErrorBoundary} from 'react-error-boundary';
 import Mypage from '../components/mypage/MyPage';
+import Fallback from '../components/Fallback';
 
 const MarketPage = () => {
   return (
-    <ErrorBoundary fallback={<div>Error!</div>}>
+    <ErrorBoundary fallback={<Fallback />}>
       <Suspense>
         <Mypage />
       </Suspense>
